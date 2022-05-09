370bf94fd68a8c9f7b9e078c1612e3f0084f1320
Rahat <rahatislamakash@gmail.com>
2022-05-09 23:33:57 +0600

    adding button decoration file

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,24 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView text;
-    TextView text2;
+    //TextView text2;
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = findViewById(R.id.button);
         text = findViewById(R.id.text);
-        text2 = findViewById(R.id.text2);
 
-        text.setText("testing the text file");
-        text.setTextColor(Color.RED);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
 
-        text2.setTextColor(Color.RED);
+                button.setBackgroundColor(Color.BLACK);
+                text.setText("Rahat");
+
+            }
+
+        });
+
+        //text = findViewById(R.id.text);
+       // text2 = findViewById(R.id.text2);
+
+       // text.setText("testing the text file");
+        //text.setTextColor(Color.RED);
+
+        //text2.setTextColor(Color.RED);
     }
 }
