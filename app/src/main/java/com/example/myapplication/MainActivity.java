72c512bce8d00fabd0fac3e724240878b3bab690
Rahat <rahatislamakash@gmail.com>
2022-05-09 16:49:27 +0600

    text file change with mainActivity,java file

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    TextView text2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text);
+        text2 = findViewById(R.id.text2);
+
+        text.setText("testing the text file");
+        text.setTextColor(Color.RED);
+
+        text2.setTextColor(Color.RED);
     }
 }
