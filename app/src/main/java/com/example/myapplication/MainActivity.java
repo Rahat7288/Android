c47f9d79f38e2c1cb3d4aa2b57ed9de7be577d62
Rahat <rahatislamakash@gmail.com>
2022-05-14 01:43:15 +0600

    learning about the shared memory saving file to local memory and display the save data on create method

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,16 +21,26 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
-    Button buttonStart;
-    Button buttonStop;
+    EditText userName;
+    EditText userMessage;
+    Button Counter;
+    CheckBox remember;
 
-    int counter = 0;
+    int count = 0;
+    String name;
+    String message;
+    boolean  isChecked;
+
+
+    SharedPreferences sharedPreferences;
+
+
     
 
 
@@ -38,27 +49,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonStart = findViewById(R.id.buttonStart);
-        buttonStop = findViewById(R.id.buttonStop);
+        userName = findViewById(R.id.editTextName);
+        userMessage = findViewById(R.id.editTextMessage);
+        Counter = findViewById(R.id.button);
+        remember = findViewById(R.id.checkBox);
 
-        buttonStart.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-
-                Intent i = new Intent(getApplicationContext(),ServicesExample.class);
-                startActivity(i);
-
-            }
-
-        });
-
-        buttonStop.setOnClickListener(new View.OnClickListener(){
+        Counter.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
 
+                count = count + 1;
+                Counter.setText(""+count);
+
             }
         });
 
+
+
+        retrieveData();
 
 
         Log.d("Message", "First Activity onCreate");
@@ -74,8 +82,65 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onPause() {
+
+        saveData();
         super.onPause();
+
         Log.d("Message", "First Activity onPause");
+    }
+
+    /*
+    * saving data ot local storage
+    * */
+
+    public void saveData(){
+        sharedPreferences =getSharedPreferences("saveData", Context.MODE_PRIVATE);
+        name = userName.getText().toString();
+        message = userMessage.getText().toString();
+        if (remember.isChecked()){
+            isChecked = true;
+        }
+        else{
+            isChecked = false;
+        }
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("Key name",name);
+        editor.putString("message",message);
+        editor.putInt("key count", count);
+        editor.putBoolean("key remember",isChecked);
+        editor.commit();
+        Toast.makeText(getApplicationContext(),"Your data is Saved",Toast.LENGTH_LONG).show();
+    }
+    /*
+    * retrieving data from the memory
+    * */
+
+    /*
+    *
+    * we have to make sure that every keyword are matched otherwise we can't get the value
+    *
+    * */
+
+    public void retrieveData(){
+        sharedPreferences = getSharedPreferences("save data",MODE_PRIVATE);
+        name = sharedPreferences.getString("Key name",null);
+        message = sharedPreferences.getString("message", null);
+        count = sharedPreferences.getInt("key count", 0);
+        isChecked = sharedPreferences.getBoolean("key remember", false);
+
+
+        userName.setText(name);
+        userMessage.setText(message);
+        Counter.setText(""+ count);
+
+        if(isChecked){
+            remember.setChecked(true);
+        }
+        else {
+            remember.setChecked(false);
+        }
+
     }
 
     @Override
